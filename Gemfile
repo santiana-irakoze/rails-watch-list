5bf9790c7e7675e99d42eb39686985990ff08665
Santiana Irakoze <gsirakoze@gmail.com>
2024-11-14 17:52:14 +0100

    done exo 2

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 ruby "3.3.5"
 
+gem "ostruct"
 gem "bootstrap", "~> 5.2"
 gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # root to "movies#home"
+  resources :lists do
+    resources :bookmarks, only: [:create]
+  end
+  resources :bookmarks, only: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
